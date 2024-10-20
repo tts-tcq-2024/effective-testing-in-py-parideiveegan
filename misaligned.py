196efd0eb1f6bb427c5299b1845248f3992aa06f
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-20 22:32:05 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -10,25 +10,6 @@ def print_color_map():
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
     
-class TestPrintColourMap(unittest.TestCase):
-     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-     def assert_stdout(self,expected_output, mock_stdout):
-          print_color_map()
-          self.asserEqual(mock_stdout.getvalue(), expected_output)
-     def test_only_numbers(self):
-          major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-          minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-          mapping_output = ""
-          for i, major in enumerate(major_colors):
-               for j, minor in enumerate(minor_colors):
-                    number = i * 5 + j
-                    number = number.rjust(2,' ')
-                    major_colour = major.ljust(6,' ')
-                    minor_colour = minor.ljust(6,' ')
-                    output_string = number+ ' | ' + major + ' | ' + minor
-                    mapping_output += output_string + '\n'       
-          self.assert_stdout(mapping_output)
-
 result = print_color_map()
 assert(result == 25)
 print("All is well (maybe!)\n")
