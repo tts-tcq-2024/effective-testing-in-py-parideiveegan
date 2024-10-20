7cd02e8e7a644704c4bbc6ceb41389969d19fdb8
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-21 00:54:06 +0530

    Update TestEnvironmentCall.py

--- TestEnvironmentCall.py
+++ TestEnvironmentCall.py
@@ -1,4 +1,8 @@
-from alerter import *
+from alerter import alert_in_celcius, alert_failure_count
 def CallMainFunctionFromTestEnv():
   alert_in_celcius(400.5)
   assert (alert_failure_count-1 < 0)
+if __name__ == "__main__":
+  CallMainFunctionFromTestEnv()
+
+  
