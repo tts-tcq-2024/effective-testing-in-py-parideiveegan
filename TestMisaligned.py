8c88c21570c385fbf913290e7d336df2dce6266c
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-20 23:00:15 +0530

    Update TestMisaligned.py

--- TestMisaligned.py
+++ TestMisaligned.py
@@ -2,6 +2,7 @@ import io
 import unittest
 import unittest.mock
 from misaligned import print_color_map
+from AlignedOutput import AlignedOutput
 
 class TestPrintColourMap(unittest.TestCase):
      @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
@@ -9,17 +10,7 @@ class TestPrintColourMap(unittest.TestCase):
           print_color_map()
           self.assertEqual(mock_stdout.getvalue(), expected_output)
      def test_print_output(self):
-          major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-          minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-          mapping_output = ""
-          for i, major in enumerate(major_colors):
-               for j, minor in enumerate(minor_colors):
-                    number = str(i * 5 + j)
-                    number = number.rjust(2,' ')
-                    major_colour = major.ljust(6,' ')
-                    minor_colour = minor.ljust(6,' ')
-                    output_string = number+ ' | ' + major_colour + ' | ' + minor_colour
-                    mapping_output += output_string + '\n'       
+          mapping_output = AlignedOutput()
           self.assert_stdout(mapping_output)
      def test_return_result(self):
           self.assertEqual(print_color_map(),25)
