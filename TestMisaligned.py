f54f479ecdb4f7a1bab2bf2f8f43ee883b37aac9
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-20 22:46:45 +0530

    Update TestMisaligned.py

--- TestMisaligned.py
+++ TestMisaligned.py
@@ -14,7 +14,7 @@ class TestPrintColourMap(unittest.TestCase):
           mapping_output = ""
           for i, major in enumerate(major_colors):
                for j, minor in enumerate(minor_colors):
-                    number = i * 5 + j
+                    number = "i * 5 + j"
                     number = number.rjust(2,' ')
                     major_colour = major.ljust(6,' ')
                     minor_colour = minor.ljust(6,' ')
